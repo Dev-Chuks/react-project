ab2b5ea1b8a1dc02be3cbe244c28cb7c1dae571a
patrick <patchamp92@gmail.com>
2024-01-20 20:08:24 -0800

    third commit

--- profile-card/src/Components/userProfileCard/userProfileCard.jsx
+++ profile-card/src/Components/userProfileCard/userProfileCard.jsx
@@ -4,21 +4,29 @@ import profile_icon from '../Asset/download.jpeg'
 
 const USERPROFILECARD = () => {
   return (
-    <div className='ups'>
+    <div> 
+           <div className='ups'>
+                <div className='image'>
+                    <img src={profile_icon} alt=''/>
+                  </div>
+                  <div className='name'>
+                    <h1>Chukwudi Patrick</h1>
+                  </div>
 
-        <div className='gradient'> </div>
-        <div className='profile-down'>
-            <img src={profile_icon} alt=''/>
-            <div className='profile-title'>Chukwudi Patrick</div> 
-            <div className='profile-description'>
-                I am a front end developer good in both html, css, javascript and reaact
-            </div>
-            <div className='profile-button'><a href="mailto:patchamp92@gmail.com">Contact me</a></div> 
+                  <div className='description'>
+                    <p>I am a front end developer good in both html, css, javascript and reaact</p>
+
+                    <a href="mailto:patchamp92@gmail.com">Contact me</a>
+
+                </div>
+           </div>
+
             
-        
-        </div>
-
     </div>
+   
+        
+
+    
   )
 }
 
